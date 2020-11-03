a0ac4e620c2a5eb8ffa7e435ee6f73ccd3f8b02c
Chris Bailey <chris@ghostly.io>
2020-11-02 23:53:56 -0500

    dialog box for adding expenses

--- apps/web/src/app/app.tsx
+++ apps/web/src/app/app.tsx
@@ -1,12 +1,20 @@
 import {
+  Button,
   Container,
+  Dialog,
+  DialogContent,
+  DialogTitle,
   Fab,
+  FormControl,
   makeStyles,
+  MenuItem,
+  Select,
   Table,
   TableBody,
   TableCell,
   TableContainer,
   TableHead,
+  TextField,
 } from '@material-ui/core';
 import AddIcon from '@material-ui/icons/Add';
 import React from 'react';
@@ -28,11 +36,34 @@ const useStyles = makeStyles((theme) => ({
     bottom: theme.spacing(2),
     right: theme.spacing(2),
   },
+  button: {
+    marginTop: theme.spacing(4),
+  },
+  formField: {
+    marginTop: theme.spacing(2),
+    width: '100%',
+  },
+  dialog: {},
 }));
 
 export const App = () => {
   const { data } = useSWR(expenseUrl, getData);
+  const [dialogOpen, setDialogOpen] = React.useState(false);
+  const [category, setCategory] = React.useState('');
   const classes = useStyles();
+
+  const handleClickOpen = () => {
+    setDialogOpen(true);
+  };
+
+  const handleClose = () => {
+    setDialogOpen(false);
+  };
+
+  const handleSelectChange = (e: React.ChangeEvent<{ value: unknown }>) => {
+    setCategory(e.target.value as string);
+  };
+
   return (
     <>
       <Header />
@@ -61,7 +92,47 @@ export const App = () => {
           </Table>
         </TableContainer>
       </Container>
-      <Fab className={classes.fab} color="primary">
+      <Dialog open={dialogOpen} onClose={handleClose} fullWidth={true}>
+        <DialogTitle id="form-dialog-title">Add Expense</DialogTitle>
+        <DialogContent>
+          <form>
+            <div>
+              <FormControl required>
+                <TextField label="Description" className={classes.formField} />
+              </FormControl>
+            </div>
+            <div>
+              <FormControl required>
+                <Select
+                  value={category}
+                  onChange={handleSelectChange}
+                  className={classes.formField}
+                >
+                  <MenuItem value="Noms">Noms</MenuItem>
+                  <MenuItem value="Pooch">Pooch</MenuItem>
+                  <MenuItem value="Billz">Billz</MenuItem>
+                  <MenuItem value="Swoll">Swoll</MenuItem>
+                </Select>
+              </FormControl>
+            </div>
+            <div>
+              <FormControl required>
+                <TextField label="Amount" className={classes.formField} />
+              </FormControl>
+            </div>
+            <div>
+              <Button
+                variant="contained"
+                color="primary"
+                className={classes.button}
+              >
+                Submit
+              </Button>
+            </div>
+          </form>
+        </DialogContent>
+      </Dialog>
+      <Fab className={classes.fab} color="primary" onClick={handleClickOpen}>
         <AddIcon />
       </Fab>
     </>
