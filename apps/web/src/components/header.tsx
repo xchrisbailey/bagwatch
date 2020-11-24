7f9216c908a6907764b61250ecae5e04541971ae
Chris Bailey <chris@ghostly.io>
2020-11-23 22:31:45 -0500

    move dialogs to landing

--- apps/web/src/components/header.tsx
+++ apps/web/src/components/header.tsx
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Dispatch, SetStateAction } from 'react';
 import {
   AppBar,
   Button,
@@ -9,8 +9,10 @@ import {
 } from '@material-ui/core';
 import { useHistory } from 'react-router-dom';
 
-import { LoginDialog } from './LoginDialog';
-import { SignupDialog } from './SignupDialog';
+interface Props {
+  signupOpen: Dispatch<SetStateAction<boolean>>;
+  loginOpen: Dispatch<SetStateAction<boolean>>;
+}
 
 const useStyles = makeStyles(() => ({
   root: {
@@ -21,9 +23,7 @@ const useStyles = makeStyles(() => ({
   },
 }));
 
-export const Header = () => {
-  const [loginDialogOpen, setLoginDialogOpen] = React.useState(false);
-  const [signupDialogOpen, setSignupDialogOpen] = React.useState(false);
+export const Header = ({ signupOpen, loginOpen }: Props) => {
   const history = useHistory();
   const classes = useStyles();
 
@@ -33,32 +33,28 @@ export const Header = () => {
   };
 
   return (
-    <>
-      <AppBar position="static">
-        <Toolbar>
-          <Typography variant="h6" className={classes.title}>
-            <Link href="/" variant="inherit" color="inherit" underline="none">
-              BAG watch
-            </Link>
-          </Typography>
-          {window.localStorage.getItem('token') ? (
-            <Button color="inherit" onClick={handleLogout}>
-              Logout
+    <AppBar position="static">
+      <Toolbar>
+        <Typography variant="h6" className={classes.title}>
+          <Link href="/" variant="inherit" color="inherit" underline="none">
+            BAG watch
+          </Link>
+        </Typography>
+        {window.localStorage.getItem('token') ? (
+          <Button color="inherit" onClick={handleLogout}>
+            Logout
+          </Button>
+        ) : (
+          <>
+            <Button color="inherit" onClick={() => loginOpen(true)}>
+              Login
             </Button>
-          ) : (
-            <>
-              <Button color="inherit" onClick={() => setLoginDialogOpen(true)}>
-                Login
-              </Button>
-              <Button color="inherit" onClick={() => setSignupDialogOpen(true)}>
-                Sign Up
-              </Button>
-            </>
-          )}
-        </Toolbar>
-      </AppBar>
-      <LoginDialog open={loginDialogOpen} setOpen={setLoginDialogOpen} />
-      <SignupDialog open={signupDialogOpen} setOpen={setSignupDialogOpen} />
-    </>
+            <Button color="inherit" onClick={() => signupOpen(true)}>
+              Sign Up
+            </Button>
+          </>
+        )}
+      </Toolbar>
+    </AppBar>
   );
 };
--- apps/web/src/pages/Landing.tsx
+++ apps/web/src/pages/Landing.tsx
@@ -1,6 +1,9 @@
 import React from 'react';
 import { Header } from '../components/header';
-import { Container, makeStyles, Typography } from '@material-ui/core';
+import { Button, Container, makeStyles, Typography } from '@material-ui/core';
+import { Link as RLink } from 'react-router-dom';
+import { LoginDialog } from '../components/LoginDialog';
+import { SignupDialog } from '../components/SignupDialog';
 
 const useStyles = makeStyles((theme) => ({
   title: {
@@ -11,10 +14,13 @@ const useStyles = makeStyles((theme) => ({
 }));
 
 const Landing = () => {
+  const [loginDialogOpen, setLoginDialogOpen] = React.useState(false);
+  const [signupDialogOpen, setSignupDialogOpen] = React.useState(false);
   const classes = useStyles();
+
   return (
     <>
-      <Header />
+      <Header loginOpen={setLoginDialogOpen} signupOpen={setSignupDialogOpen} />
       <Container maxWidth="md">
         <Typography variant="h2" color="primary" className={classes.title}>
           Bag Watch
@@ -24,11 +30,37 @@ const Landing = () => {
           Expense tracking made simple, and pretty
         </Typography>
         {localStorage.getItem('token') ? (
-          <p>go to dashboard</p>
+          <Button
+            variant="contained"
+            component={RLink}
+            color="primary"
+            to="/dashboard"
+          >
+            load your dashboard
+          </Button>
         ) : (
-          <p>login or signup</p>
+          <>
+            <Button
+              variant="contained"
+              color="primary"
+              style={{ marginRight: '1rem' }}
+              onClick={() => setLoginDialogOpen(true)}
+            >
+              Login
+            </Button>
+            <Button
+              variant="contained"
+              color="primary"
+              onClick={() => setSignupDialogOpen(true)}
+            >
+              Sign Up
+            </Button>
+          </>
         )}
       </Container>
+
+      <LoginDialog open={loginDialogOpen} setOpen={setLoginDialogOpen} />
+      <SignupDialog open={signupDialogOpen} setOpen={setSignupDialogOpen} />
     </>
   );
 };
